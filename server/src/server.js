564bb66d30de7d1cf0ae081c2e3a85e5b1e86f2b
thachdinh110902@gmail.com <thachdinh@ThachNhe>
2024-08-05 14:49:06 +0700

    sequelize + docker compose

--- server/src/server.js
+++ server/src/server.js
@@ -4,12 +4,12 @@ import initWebRoute from './routes/web';
 import connectDB from './config/connectDB';
 require('dotenv').config();
 const PORT = process.env.PORT || 8080;
-const app = express();
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb' }));
+const app1 = express();
+app1.use(bodyParser.json({ limit: '50mb' }));
+app1.use(bodyParser.urlencoded({ limit: '50mb' }));
 connectDB();
-initWebRoute(app);
+initWebRoute(app1);
 
-app.listen(PORT, () => {
+app1.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`);
 });
